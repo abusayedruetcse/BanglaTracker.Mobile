4190688b1cf2662330f3e75924542707c71214be
algoBhaiya <abusayedahmadruet13@gmail.com>
2024-11-07 22:32:21 +0600

    #34 Allow single click for search button

---
+++ src/BanglaTracker.Presentation/Services/NavigationService.cs
@@ -0,0 +1,25 @@
+﻿using BanglaTracker.Presentation.Interfaces;
+
+namespace BanglaTracker.Presentation.Services
+{
+    public class NavigationService : INavigationService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public NavigationService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task NavigateToAsync<TPage>() where TPage : Page
+        {
+            var page = _serviceProvider.GetRequiredService<TPage>();
+            
+            if (page != null && Application.Current?.MainPage?.Navigation != null)
+            {
+                await Application.Current.MainPage.Navigation.PushAsync(page);
+            }
+        }
+    }
+
+}
